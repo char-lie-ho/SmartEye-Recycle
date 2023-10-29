db55dd56eff364ff798a8da1f0370e9219e6a1d6
ho921 <tho124@my.bcit.ca>
2023-10-28 22:19:38 -0700

    add sticky footer

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -5,14 +5,13 @@ function loadSkeleton() {
             // User is signed in.
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('../text/nav_after_login.html'));
-            // console.log($('#footerPlaceholder').load('./text/footer.html'));  ///comment this out because not sure if we need a footer
+            console.log($('#footerPlaceholder').load('../text/footer.html'));  
         } else {
             // No user is signed in.
             console.log($('#navbarPlaceholder').load('../text/nav_before_login.html'));
-            // console.log($('#footerPlaceholder').load('./text/footer.html')); ///comment this out because not sure if we need a footer
+            console.log($('#footerPlaceholder').load('../text/footer.html')); 
         }
     });
 }
 loadSkeleton(); //invoke the function
 
-console.log('hello')

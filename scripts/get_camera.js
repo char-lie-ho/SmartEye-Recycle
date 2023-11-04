7e04d2c404a5a5d64f937fe3f0f3eda566f3aed8
ho921 <tho124@my.bcit.ca>
2023-11-04 10:48:51 -0700

    create functional scanner, add buttons

--- scripts/get_camera.js
+++ scripts/get_camera.js
@@ -1,13 +1,40 @@
-let video = document.querySelector("#videoElement");
+let codeReader = null;
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true})
-  .then(function (stream) {
-    video.srcObject = stream;
-  })
-  .catch (function (error) {
-    console.log("something went wrong!");
-  })
-} else {
-  console.log("getUserMedia is not supported");
+function startScanning() {
+  codeReader = new ZXing.BrowserQRCodeReader();
+  codeReader.decodeFromVideoDevice(null, 'webcam-preview', (result, err) => {
+    if (result) {
+      console.log('Found QR code!', result)
+      document.getElementById('result').textContent = result.text
+    }
+
+    if (err) {
+      // As long as this error belongs into one of the following categories
+      // the code reader is going to continue as excepted. Any other error
+      // will stop the decoding loop.
+      if (err instanceof ZXing.NotFoundException) {
+        console.log('No QR code found.')
+      }
+
+      if (err instanceof ZXing.ChecksumException) {
+        console.log('A code was found, but it\'s read value was not valid.')
+      }
+
+      if (err instanceof ZXing.FormatException) {
+        console.log('A code was found, but it was in an invalid format.')
+      }
+    }
+  });
 }
+
+function stopScanning() {
+  if (codeReader) {
+    codeReader.reset();
+  }
+}
+//pause scanning
+document.getElementById('stopButton').addEventListener('click', stopScanning);
+//restart scanning
+document.getElementById('restartButton').addEventListener('click', startScanning);
+// Start scanning initially
+startScanning(); 

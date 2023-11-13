4c3ef2fca6c66fc52f864eba9e15957859f6f86a
ho921 <tho124@my.bcit.ca>
2023-11-12 20:53:47 -0800

    Finish dynamic loading on search_instruction

--- scripts/search.js
+++ scripts/search.js
@@ -2,3 +2,29 @@
 function goBack() {
     window.history.back();
 }
+
+// read from database
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("eachCategoriesTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allCategories => {
+            allCategories.forEach(doc => { //iterate thru each doc
+                var title = doc.data().categoryDocID;       // get value of the "name" key
+                var imageCode = doc.data().imagecode;
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-img').src = `../images/${imageCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "category_template.html?docID=" + docID;
+                console.log(title)
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById("categories-goes-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("category");  //input param is the name of the collection

713b59ad019619ab58ffc02f728c21dbbb8cad3f
ho921 <tho124@my.bcit.ca>
2023-11-12 20:01:24 -0800

    Polish the category search

--- scripts/eachCategory.js
+++ scripts/eachCategory.js
@@ -1,19 +1,21 @@
 // Reference to the 'category' collection, then the 'paper_cardboard' subcollection
-const categoryRef = db.collection('category').doc('paper_cardboard');
+const categoryRef = db.collection('category').doc('_______'); //use the document ID
 
 // Query the 'instruction' and 'recyclable' fields
 categoryRef.get()
     .then((doc) => {
         if (doc.exists) {
+            const title = doc.data().categoryDocID;
             const instruction = doc.data().instruction;
             const recyclable = doc.data().recyclable;
             const examples = doc.data().examples;
             const image_url = doc.data().img;
             console.log(`Instruction: ${instruction}, Recyclable: ${recyclable}`);
+            document.getElementById("material").innerText = title;
             document.getElementById("instruction-go-here").innerText = instruction;
             document.getElementById("recyclable-go-here").innerText = recyclable;
             document.getElementById("example-go-here").innerText = examples;
-            document.getElementById('paper').setAttribute('src', image_url);
+            document.getElementById('material_image').setAttribute('src', image_url);
 
             console.log(image_url)
         } else {

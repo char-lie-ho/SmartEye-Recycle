71bde08aa5c5976c2cc1927a08663e55f589a9c1
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-12 21:11:19 -0800

    Display alarm time

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -2,3 +2,16 @@
 function goBack() {
     window.history.back();
 }
+
+function submitTime() {
+    console.log('hi there')
+    let alarmTime = document.getElementById('alarmtime').value;
+    let newAlarmTime = new Date(alarmTime);
+    
+    var year = newAlarmTime.getFullYear();
+    var month = newAlarmTime.getMonth() + 1; // 月份是从0开始的，所以要加1
+    var day = newAlarmTime.getDate();
+    var hours = newAlarmTime.getHours();
+    var minutes = newAlarmTime.getMinutes();
+    document.getElementById('alarm-goes-here').innerHTML = `<div>${year} - ${month} - ${day} - ${hours} - ${minutes}</div>`
+}

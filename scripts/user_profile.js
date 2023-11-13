8d136277d5786b24e40c03227dee89a3ff9edbd0
ho921 <tho124@my.bcit.ca>
2023-11-13 08:29:19 -0800

    Fix a bug, now the fields are loading

--- scripts/user_profile.js
+++ scripts/user_profile.js
@@ -3,77 +3,38 @@ function goBack() {
     window.history.back();
 }
 
-//get user name
-function getNameFromAuth() {
+// Get user name and email
+function getUserInfoFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            userName = user.displayName;
-
-            //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;
-
-            //method #2:  insert using jquery
-            //$("#name-goes-here").text(userName); //using jquery
-
-            //method #3:  insert using querySelector
-            //document.querySelector("#name-goes-here").innerText = userName
-
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getNameFromAuth(); //run the function
-
-
-//get user email address
-function getEmailFromAuth() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.email);
-            userEmail = user.email;
-            //method #1:  insert with JS
-            document.getElementById("email-goes-here").innerText = userEmail;
-        } else {
-            // No user is signed in.
-        }
-    });
-}
-getEmailFromAuth(); //run the function
-
-
-var currentUser;               //points to the document of the user who is logged in
-function populateUserInfo() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
-        if (user) {
-
-            //go to the correct user document by referencing to the user uid
             currentUser = db.collection("users").doc(user.uid)
             //get the document for current user.
             currentUser.get()
                 .then(userDoc => {
-                    //get the data fields of the user
-                    var userName = userDoc.data().name;
-                    var userEmail = userDoc.data().email;
-                    var userCity = userDoc.data().city;
+                    // Do something for the currently logged-in user here: 
+                    console.log(user.uid); // Print the uid in the browser console
+                    console.log(user.displayName); // Print the user name in the browser console
+                    console.log(user.email); // Print the email in the browser console
 
-                    //if the data fields are not empty, then write them in to the form.
+                    // Get user information
+                    userName = user.displayName;
+                    userEmail = user.email;
+
+
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
                     }
-                    if (userCity != null) {
+                    if (userEmail != null) {
                         document.getElementById("emailInput").value = userEmail;
                     }
-                    if (userCity != null) {
-                        document.getElementById("cityInput").value = userCity;
-                    }
+
+                    // Insert user name using JS
+                    document.getElementById("name-goes-here").innerText = userName;
+
+                    // Insert email using JS
+                    document.getElementById("email-goes-here").innerText = userEmail;
+
                 })
         } else {
             // No user is signed in.
@@ -81,6 +42,9 @@ function populateUserInfo() {
         }
     });
 }
+
+getUserInfoFromAuth(); // Run the function
+
 function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
@@ -100,5 +64,5 @@ currentUser.update({
     .then(() => {
         console.log("Document successfully updated!");
     })
-//call the function to run it 
+//call the function to run it
 populateUserInfo();

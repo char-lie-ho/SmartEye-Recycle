f5c7647723866e521bb6b6cc8747afb78c7c197b
ho921 <tho124@my.bcit.ca>
2023-11-16 23:45:09 -0800

    Modify settings of scanner

--- scripts/get_camera.js
+++ scripts/get_camera.js
@@ -10,20 +10,20 @@ function startScanner() {
       constraints: {
         width: 320,
         height: 370,
-        facingMode: "environment"
+        facingMode: "user"
       },
     },
     decoder: {
       readers: [
-        "code_128_reader",
+        // "code_128_reader",
         "ean_reader",
-        "ean_8_reader",
-        "code_39_reader",
-        "code_39_vin_reader",
-        "codabar_reader",
-        "upc_reader",
-        "upc_e_reader",
-        "i2of5_reader"
+        // "ean_8_reader",
+        // "code_39_reader",
+        // "code_39_vin_reader",
+        // "codabar_reader",
+        // "upc_reader",
+        // "upc_e_reader",
+        // "i2of5_reader"
       ],
       debug: {
         showCanvas: true,

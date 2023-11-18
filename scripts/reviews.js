2be331b46b146cd068bc3cd564e78430ae1c49c7
ho921 <tho124@my.bcit.ca>
2023-11-18 15:24:45 -0800

    Add user name in the review

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,5 +1,4 @@
 //To handle user interactions and update the rating value
-
 $(document).ready(function () {
     $('.star').click(function () {
         $(this).addClass('active');
@@ -18,15 +17,16 @@ $(document).ready(function () {
 var facilityID = localStorage.getItem("facilityID");
 // console.log(facilityID)
 
+
+// display the facility name 
 function getFacilityName(id) {
     db.collection("facility")
         .doc(id)
         .get()
         .then((facilities) => {
             var facilityName = facilities.data().name
-            document.getElementById('facilityName').innerHTML = facilityName 
-        }
-        )
+            document.getElementById('facilityName').innerHTML = facilityName
+        })
 }
 
 getFacilityName(facilityID);
@@ -38,8 +38,7 @@ function submitReview() {
     var review = db.collection("reviews");
     // collect the checked boxes in Types of material
     const selectedValues = [];
-    document.querySelectorAll('[id="materials"]:checked').forEach((checkbox) => {selectedValues.push(checkbox.value)});
-
+    document.querySelectorAll('[id="materials"]:checked').forEach((checkbox) => { selectedValues.push(checkbox.value) });
     review.add({
         facilityID: facilityID,
         materialsHandle: selectedValues,
@@ -48,6 +47,7 @@ function submitReview() {
         overallRating: document.getElementById('rating-value').innerHTML,
         recommended: document.querySelector('input[name="recommend"]:checked').value,
         comment: document.getElementById('comment').value,
+        user: `${userName}`,
         last_updated: firebase.firestore.FieldValue.serverTimestamp()
     })
         .then(function () {
@@ -66,3 +66,23 @@ document.getElementById('submit_review').addEventListener('click', function () {
 function goBack() {
     window.history.back();
 }
+
+// set the default to anonymous
+var userName = 'anonymous';
+
+// Obtain the current user name
+function getUserInfoFromAuth() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid)
+            currentUser.get()
+                .then(() => {
+                    userName = user.displayName
+                    // console.log(user.displayName)
+                })
+        }
+    })
+}
+
+getUserInfoFromAuth(); // Run the function

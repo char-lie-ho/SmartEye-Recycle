bb7e11f5ab085547bfd6daceaadd96390afc0474
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-27 09:51:14 -0800

    Make heart icon functional

--- scripts/nearby_facility.js
+++ scripts/nearby_facility.js
@@ -19,15 +19,18 @@ function displayCardsDynamically(collection) {
                 eachcard.querySelector('.card-image').src = image_url
                 //load the appropriate facility
                 eachcard.querySelector('a').href = "facility_template.html?docID=" + facilityID;
+                eachcard.querySelector('i').id = "fav" + facilityID;
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById("facilities-goes-here").appendChild(eachcard);
 
                 //i++;   //Optional: iterate variable to serve as unique ID
 
-                var favoriteButton = document.getElementById("favoriteButton");
+                var favoriteButton = document.getElementById("fav" + facilityID);
                 favoriteButton.addEventListener("click", function () {
-                    this.classList.toggle("filled");
+                    document.getElementById("fav" + facilityID).innerHTML = `<i class="bi bi-heart-fill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+</svg></i>`
                 })
             });
 

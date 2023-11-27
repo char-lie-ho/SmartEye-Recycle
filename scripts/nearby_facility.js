1ddaadd70b9a6fb7ac409d5272f6d2111297223a
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-27 15:46:36 -0800

    This is test

--- scripts/nearby_facility.js
+++ scripts/nearby_facility.js
@@ -28,6 +28,7 @@ function displayCardsDynamically(collection) {
 
                 var favoriteButton = document.getElementById("fav" + facilityID);
                 favoriteButton.addEventListener("click", function () {
+                    // This is a test
                     document.getElementById("fav" + facilityID).innerHTML = `<i class="bi bi-heart-fill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
 </svg></i>`

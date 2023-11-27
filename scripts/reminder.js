191f74a670fa6e7779099c4c07ff88dd1928ec71
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-26 19:37:50 -0800

    Make the page refresh once saving the time

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -45,7 +45,7 @@ function submitTime() {
 }
 
 //save the time into database
-function saveInToDatabase() {
+async function saveInToDatabase() {
     if (submitTime()) {
         const user = firebase.auth().currentUser;
         if (user) {
@@ -54,16 +54,17 @@ function saveInToDatabase() {
             remindTime = localStorage.getItem('alarmTime');
 
             // Get the document for the current user.
-            currentUser.update({
+            await currentUser.update({
                 remindTime: firebase.firestore.FieldValue.arrayUnion(remindTime),
                 timestamp: firebase.firestore.FieldValue.serverTimestamp()
             }).then(() => {
-                Swal.fire('You have saved the alarm!');
+                window.location.reload();
             });
         } else {
             console.log("No user is signed in");
             window.location.href = 'login.html';
         }
+        window.location.reload()
     }
 }
 
@@ -100,13 +101,8 @@ function displayRemindTime() {
                             let list_items = document.createElement('li');
                             // write text into <li>tag
                             list_items.textContent = time;
-                            //                             trash_icon = document.createElement('span')
-                            //                             trash_icon.innerText = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            //                                 < path d = "M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                            //                                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                            // </svg >`
-
-                            // append <li> into <ol>
+                                                    
+                            
                             document.getElementById('alarm_list').appendChild(list_items)
                             trash = document.createElement('span')
                             trash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">

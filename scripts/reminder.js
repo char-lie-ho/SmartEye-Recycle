1c68f410afd426cae8ee2cee19da98f975b326ee
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-26 19:04:26 -0800

    Sync alarm time once save

--- scripts/reminder.js
+++ scripts/reminder.js
@@ -31,7 +31,16 @@ function submitTime() {
     selectedTime = `${year} - ${month} - ${day} - ${hours} : ${minutes}`
 
     localStorage.setItem('alarmTime', selectedTime) // save the selected time into application
-    document.getElementById('alarm-goes-here').innerHTML = `<div>${selectedTime}</div>` // change the alarm time without refreshing
+
+    document.getElementById('alarm_list').innerHTML += `
+    <li>
+    ${selectedTime}
+    <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                < path d = "M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+</svg ></span>
+    </li>
+    ` // change the alarm time without refreshing
     return true
 }
 
@@ -60,7 +69,7 @@ function saveInToDatabase() {
 
 
 // Delete the selected time from database
-function cleandata(time, userDocRef){
+function cleandata(time, userDocRef) {
     userDocRef.update({
         remindTime: firebase.firestore.FieldValue.arrayRemove(time),
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
@@ -106,7 +115,7 @@ function displayRemindTime() {
 </svg >`
                             trash.addEventListener('click', function () {
                                 list_items.remove()
-                                
+
                                 //TODO: delete from database
                                 cleandata(time, userDocRef);
 

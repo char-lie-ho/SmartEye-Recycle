1e55514f226da0c11a463ae94b65f3efd0928310
ho921 <tho124@my.bcit.ca>
2023-11-29 12:31:53 -0800

    Finish displaying favorite facility

--- scripts/favourite_facility.js
+++ scripts/favourite_facility.js
@@ -2,3 +2,96 @@
 function goBack() {
     window.history.back();
 }
+
+// read from database
+function displayCardsDynamically(collection, favoriteList) {
+
+    for (eachFavorite in favoriteList) {
+        let cardTemplate = document.getElementById("facilityTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+        const favFacility = db.collection('facility')
+        favFacility.where('name', '==', favoriteList[eachFavorite]) // pass the names of the facility from user, replace collection
+            .get()
+            .then(allFacilities => {
+                allFacilities.forEach(doc => { //iterate thru each doc
+                    var title = doc.data().name;
+                    var address = doc.data().address;
+                    var facilityID = doc.id;
+                    var image_url = doc.data().image;
+                    let eachcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (eachcard) that will be filled with Firestore data.
+
+                    //update title and text and image
+                    eachcard.querySelector('.card-title').innerHTML = title;
+                    eachcard.querySelector('.card-address').innerHTML = address;
+                    eachcard.querySelector('.card-image').src = image_url
+                    //load the appropriate facility
+                    eachcard.querySelector('a').href = "facility_template.html?docID=" + facilityID;
+                    // eachcard.querySelector('i').id = facilityID;
+                    eachcard.querySelector('i').id = title
+
+                    //append each facility 
+                    document.getElementById("facilities-goes-here").appendChild(eachcard);
+
+                    //read from DB, ensure favorite icon is correct color
+                    currentUser.get().then(userDoc => {
+                        var favorite = userDoc.data().favorite;
+                        if (favorite.includes(title)) {
+                            document.getElementById(title).style = "font-variation-settings: 'FILL' 1; color: red;"
+                        }
+                    })
+                    //favortie button eventListener
+                    var favoriteButton = document.getElementById(title);
+                    favoriteButton.addEventListener("click", function () {
+                        console.log(title)
+                        updateFavourite(title)
+                    })
+                });
+            })
+    }
+}
+
+var currentUser;
+
+var favoriteBtn = document.querySelectorAll('.btn btn card-href')
+
+// update the color of favorite button when clicked 
+function updateFavourite(title) {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            const user = firebase.auth().currentUser;
+            currentUser.get().then(userDoc => {
+                var favorite = userDoc.data().favorite;
+                let isFavorite = favorite.includes(title)
+                if (isFavorite) {
+                    currentUser.update({
+                        favorite: firebase.firestore.FieldValue.arrayRemove(title)
+                    }).then(function () {
+                        document.getElementById(title).style = "font-variation-settings: 'FILL' 0; color: red;"
+                    })
+                } else {
+                    currentUser.update({
+                        favorite: firebase.firestore.FieldValue.arrayUnion(title)
+                    }).then(function () {
+                        document.getElementById(title).style = "font-variation-settings: 'FILL' 1; color: red;"
+                    })
+                }
+            })
+        }
+    })
+}
+
+
+function main() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            const user = firebase.auth().currentUser;
+            currentUser = db.collection("users").doc(user.uid); //global
+            currentUser.get().then(userDoc => {
+                var favoriteList = userDoc.data().favorite;
+                displayCardsDynamically("facility", favoriteList)  //input param is the name of the collection
+            })
+
+        }
+    })
+}
+
+main()

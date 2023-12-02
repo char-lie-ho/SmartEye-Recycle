3c0ec961d29aca147911dd16b8b32fd088e7c07f
ho921 <tho124@my.bcit.ca>
2023-12-01 23:24:05 -0800

    Combine main.js with greeting.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,23 @@ function getNameFromAuth() {
     });
 }
 getNameFromAuth(); //run the function
+
+
+// greeting message
+var thehours = new Date().getHours();
+var themessage;
+var morning = ('Good morning,');
+var afternoon = ('Good afternoon,');
+var evening = ('Good evening,');
+
+if (thehours >= 0 && thehours < 12) {
+    themessage = morning;
+
+} else if (thehours >= 12 && thehours < 17) {
+    themessage = afternoon;
+
+} else if (thehours >= 17 && thehours < 24) {
+    themessage = evening;
+}
+
+$('.greeting').append(themessage);
